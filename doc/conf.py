7cc9b7f1eccde995a78ca79704f5c43f1e82a3d1
Pavel Korshunov <pavel.korshunov@idiap.ch>
2016-10-11 11:13:17 +0200

    [sphinx] Fixed Warnings

--- doc/conf.py
+++ doc/conf.py
@@ -131,7 +131,7 @@ pygments_style = 'sphinx'
 
 # Some variables which are useful for generated material
 project_variable = project.replace('.', '_')
-short_description = u'Audio voicePA Attack Database Access API for Bob'
+short_description = u'voicePA Speech Presentation Attack Database API for Bob'
 owner = [u'Idiap Research Institute']
 
 
