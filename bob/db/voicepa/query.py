246ea51401040571aa88cf40b991b55080ae1398
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2017-05-11 16:38:14 +0200

    The SQLiteDatabase now accepts original_directory and original_extension

--- bob/db/voicepa/query.py
+++ bob/db/voicepa/query.py
@@ -25,9 +25,10 @@ class Database(bob.db.base.SQLiteDatabase):
     and for the data itself inside the database.
     """
 
-    def __init__(self):
+    def __init__(self, original_directory=None, original_extension=None):
         # opens a session to the database - keep it open until the end
-        super(Database, self).__init__(SQLITE_FILE, File)
+        super(Database, self).__init__(SQLITE_FILE, File,
+                                       original_directory, original_extension)
 
     def objects(self, protocol='grandtest', attack_data=File.attack_data_choices,
                 groups=Client.group_choices, cls=('real',), recording_devices=File.recording_device_choices,
@@ -87,47 +88,58 @@ class Database(bob.db.base.SQLiteDatabase):
 
         # check if groups set are valid
         VALID_GROUPS = self.groups()
-        groups = self.check_parameters_for_validity(groups, "group", VALID_GROUPS, None)
+        groups = self.check_parameters_for_validity(
+            groups, "group", VALID_GROUPS, None)
 
         # check if groups set are valid
         VALID_GENDER = self.genders()
-        gender = self.check_parameters_for_validity(gender, "gender", VALID_GENDER, None)
+        gender = self.check_parameters_for_validity(
+            gender, "gender", VALID_GENDER, None)
 
         # check if supports set are valid
         VALID_SUPPORTS = self.attack_datas()
-        attack_data = self.check_parameters_for_validity(attack_data, "attack_data", VALID_SUPPORTS, None)
+        attack_data = self.check_parameters_for_validity(
+            attack_data, "attack_data", VALID_SUPPORTS, None)
 
         # check if supports set are valid
         VALID_ATTACKDEVICES = self.attack_devices()
-        attack_devices = self.check_parameters_for_validity(attack_devices, "attack_device", VALID_ATTACKDEVICES, None)
+        attack_devices = self.check_parameters_for_validity(
+            attack_devices, "attack_device", VALID_ATTACKDEVICES, None)
 
         # check if supports set are valid
         VALID_ASVDEVICES = self.asv_devices()
-        asv_devices = self.check_parameters_for_validity(asv_devices, "asv_device", VALID_ASVDEVICES, None)
+        asv_devices = self.check_parameters_for_validity(
+            asv_devices, "asv_device", VALID_ASVDEVICES, None)
 
         # check if supports set are valid
         VALID_ENVIRONMENTS = self.environments()
-        environments = self.check_parameters_for_validity(environments, "environment", VALID_ENVIRONMENTS, None)
+        environments = self.check_parameters_for_validity(
+            environments, "environment", VALID_ENVIRONMENTS, None)
 
         # by default, do NOT grab enrollment data from the database
         VALID_CLASSES = ('real', 'attack', 'enroll', 'probe')
-        cls = self.check_parameters_for_validity(cls, "class", VALID_CLASSES, VALID_CLASSES)
+        cls = self.check_parameters_for_validity(
+            cls, "class", VALID_CLASSES, VALID_CLASSES)
 
         # check protocol validity
         VALID_PROTOCOLS = [k.name for k in self.protocols()]
-        protocol = self.check_parameters_for_validity(protocol, "protocol", VALID_PROTOCOLS, ('grandtest',))
+        protocol = self.check_parameters_for_validity(
+            protocol, "protocol", VALID_PROTOCOLS, ('grandtest',))
 
         # checks client identity validity
         VALID_CLIENTS = [k.id for k in self.clients()]
-        clients = self.check_parameters_for_validity(clients, "client", VALID_CLIENTS, None)
+        clients = self.check_parameters_for_validity(
+            clients, "client", VALID_CLIENTS, None)
 
         # checks if the device is valid
         VALID_DEVICES = self.devices()
-        recording_devices = self.check_parameters_for_validity(recording_devices, "recording_device", VALID_DEVICES, None)
+        recording_devices = self.check_parameters_for_validity(
+            recording_devices, "recording_device", VALID_DEVICES, None)
 
         # checks if the device is valid
         VALID_SESSIONS = self.sessions()
-        sessions = self.check_parameters_for_validity(sessions, "session", VALID_SESSIONS, None)
+        sessions = self.check_parameters_for_validity(
+            sessions, "session", VALID_SESSIONS, None)
 
         # now query the database
         retval = []
@@ -136,56 +148,90 @@ class Database(bob.db.base.SQLiteDatabase):
         purpose = ('real',)
         if 'real' in cls:  # the whole real data
             # init the query
-            q = self.m_session.query(File).join(ProtocolFiles).join((Protocol, ProtocolFiles.protocol)).join(Client)
-            if groups: q = q.filter(Client.group.in_(groups))
-            if clients: q = q.filter(Client.id.in_(clients))
-            if gender: q = q.filter(Client.gender.in_(gender))
-            if recording_devices: q = q.filter(File.recording_device.in_(recording_devices))
-            if sessions: q = q.filter(File.session.in_(sessions))
-            if attack_data: q = q.filter(File.attack_data.in_(attack_data))
-            if attack_devices: q = q.filter(File.attack_device.in_(attack_devices))
-            if asv_devices:  q = q.filter(File.asv_device.in_(asv_devices))
-            if environments:  q = q.filter(File.environment.in_(environments))
+            q = self.m_session.query(File).join(ProtocolFiles).join(
+                (Protocol, ProtocolFiles.protocol)).join(Client)
+            if groups:
+                q = q.filter(Client.group.in_(groups))
+            if clients:
+                q = q.filter(Client.id.in_(clients))
+            if gender:
+                q = q.filter(Client.gender.in_(gender))
+            if recording_devices:
+                q = q.filter(File.recording_device.in_(recording_devices))
+            if sessions:
+                q = q.filter(File.session.in_(sessions))
+            if attack_data:
+                q = q.filter(File.attack_data.in_(attack_data))
+            if attack_devices:
+                q = q.filter(File.attack_device.in_(attack_devices))
+            if asv_devices:
+                q = q.filter(File.asv_device.in_(asv_devices))
+            if environments:
+                q = q.filter(File.environment.in_(environments))
             q = q.filter(File.purpose.in_(purpose))
             q = q.filter(Protocol.name.in_(protocol))
             q = q.order_by(File.path)
             retval += list(q)
 
-        if 'enroll' in cls:  # if we need enroll data (a small subset of real data)
+        # if we need enroll data (a small subset of real data)
+        if 'enroll' in cls:
             # init the query
-            q = self.m_session.query(File).join(ProtocolFiles).join((Protocol, ProtocolFiles.protocol)).join(Client)
+            q = self.m_session.query(File).join(ProtocolFiles).join(
+                (Protocol, ProtocolFiles.protocol)).join(Client)
             from sqlalchemy import and_
             # only data from sess1 and laptop is in enrollment
-            q = q.filter(and_(File.recording_device == 'laptop', File.session == 'sess1'))
-            if groups: q = q.filter(Client.group.in_(groups))
-            if clients: q = q.filter(Client.id.in_(clients))
-            if gender: q = q.filter(Client.gender.in_(gender))
-            if recording_devices: q = q.filter(File.recording_device.in_(recording_devices))
-            if sessions: q = q.filter(File.session.in_(sessions))
-            if attack_data: q = q.filter(File.attack_data.in_(attack_data))
-            if attack_devices: q = q.filter(File.attack_device.in_(attack_devices))
-            if asv_devices:  q = q.filter(File.asv_device.in_(asv_devices))
-            if environments:  q = q.filter(File.environment.in_(environments))
+            q = q.filter(and_(File.recording_device ==
+                              'laptop', File.session == 'sess1'))
+            if groups:
+                q = q.filter(Client.group.in_(groups))
+            if clients:
+                q = q.filter(Client.id.in_(clients))
+            if gender:
+                q = q.filter(Client.gender.in_(gender))
+            if recording_devices:
+                q = q.filter(File.recording_device.in_(recording_devices))
+            if sessions:
+                q = q.filter(File.session.in_(sessions))
+            if attack_data:
+                q = q.filter(File.attack_data.in_(attack_data))
+            if attack_devices:
+                q = q.filter(File.attack_device.in_(attack_devices))
+            if asv_devices:
+                q = q.filter(File.asv_device.in_(asv_devices))
+            if environments:
+                q = q.filter(File.environment.in_(environments))
             q = q.filter(File.purpose.in_(purpose))
             q = q.filter(Protocol.name.in_(protocol))
             q = q.order_by(File.path)
             retval += list(q)
 
-        if 'probe' in cls:  # if we need probe data (a large subset of real data)
+        # if we need probe data (a large subset of real data)
+        if 'probe' in cls:
             # init the query
-            q = self.m_session.query(File).join(ProtocolFiles).join((Protocol, ProtocolFiles.protocol)).join(Client)
+            q = self.m_session.query(File).join(ProtocolFiles).join(
+                (Protocol, ProtocolFiles.protocol)).join(Client)
             from sqlalchemy import or_
             # all data except the one from sess1 and laptop
-            q = q.filter(or_(File.recording_device != 'laptop', File.session != 'sess1'))
-            if groups: q = q.filter(Client.group.in_(groups))
-            if clients: q = q.filter(Client.id.in_(clients))
-            if gender: q = q.filter(Client.gender.in_(gender))
-            if recording_devices: q = q.filter(File.recording_device.in_(recording_devices))
-            if sessions: q = q.filter(File.session.in_(sessions))
-            if attack_data: q = q.filter(File.attack_data.in_(attack_data))
-            if attack_devices: q = q.filter(File.attack_device.in_(attack_devices))
-            if asv_devices:  q = q.filter(File.asv_device.in_(asv_devices))
-            if environments:  q = q.filter(File.environment.in_(environments))
+            q = q.filter(or_(File.recording_device !=
+                             'laptop', File.session != 'sess1'))
+            if groups:
+                q = q.filter(Client.group.in_(groups))
+            if clients:
+                q = q.filter(Client.id.in_(clients))
+            if gender:
+                q = q.filter(Client.gender.in_(gender))
+            if recording_devices:
+                q = q.filter(File.recording_device.in_(recording_devices))
+            if sessions:
+                q = q.filter(File.session.in_(sessions))
+            if attack_data:
+                q = q.filter(File.attack_data.in_(attack_data))
+            if attack_devices:
+                q = q.filter(File.attack_device.in_(attack_devices))
+            if asv_devices:
+                q = q.filter(File.asv_device.in_(asv_devices))
+            if environments:
+                q = q.filter(File.environment.in_(environments))
             q = q.filter(File.purpose.in_(purpose))
             q = q.filter(Protocol.name.in_(protocol))
             q = q.order_by(File.path)
@@ -194,17 +240,28 @@ class Database(bob.db.base.SQLiteDatabase):
         if 'attack' in cls:
             purpose = ('attack',)
             # init the query
-            q = self.m_session.query(File).join(ProtocolFiles).join((Protocol, ProtocolFiles.protocol)).join(Client)
-            # if both enroll and probe data is requested, then do not do anything
-            if groups: q = q.filter(Client.group.in_(groups))
-            if clients: q = q.filter(Client.id.in_(clients))
-            if gender: q = q.filter(Client.gender.in_(gender))
-            if attack_data: q = q.filter(File.attack_data.in_(attack_data))
-            if attack_devices: q = q.filter(File.attack_device.in_(attack_devices))
-            if asv_devices:  q = q.filter(File.asv_device.in_(asv_devices))
-            if environments:  q = q.filter(File.environment.in_(environments))
-            if recording_devices: q = q.filter(File.recording_device.in_(recording_devices))
-            if sessions: q = q.filter(File.session.in_(sessions))
+            q = self.m_session.query(File).join(ProtocolFiles).join(
+                (Protocol, ProtocolFiles.protocol)).join(Client)
+            # if both enroll and probe data is requested, then do not do
+            # anything
+            if groups:
+                q = q.filter(Client.group.in_(groups))
+            if clients:
+                q = q.filter(Client.id.in_(clients))
+            if gender:
+                q = q.filter(Client.gender.in_(gender))
+            if attack_data:
+                q = q.filter(File.attack_data.in_(attack_data))
+            if attack_devices:
+                q = q.filter(File.attack_device.in_(attack_devices))
+            if asv_devices:
+                q = q.filter(File.asv_device.in_(asv_devices))
+            if environments:
+                q = q.filter(File.environment.in_(environments))
+            if recording_devices:
+                q = q.filter(File.recording_device.in_(recording_devices))
+            if sessions:
+                q = q.filter(File.session.in_(sessions))
             q = q.filter(File.purpose.in_(purpose))
             q = q.filter(Protocol.name.in_(protocol))
             q = q.order_by(File.path)
@@ -229,10 +286,14 @@ class Database(bob.db.base.SQLiteDatabase):
 
         Returns: A list containing the ids of all models belonging to the given group.
         """
-        if protocol == '.': protocol = None
-        protocol = self.check_parameters_for_validity(protocol, "protocol", self.protocol_names(), None)
-        groups = self.check_parameters_for_validity(groups, "group", self.groups(), self.groups())
-        gender = self.check_parameters_for_validity(gender, "gender", self.genders(), None)
+        if protocol == '.':
+            protocol = None
+        protocol = self.check_parameters_for_validity(
+            protocol, "protocol", self.protocol_names(), None)
+        groups = self.check_parameters_for_validity(
+            groups, "group", self.groups(), self.groups())
+        gender = self.check_parameters_for_validity(
+            gender, "gender", self.genders(), None)
 
         retval = []
         if groups:
@@ -328,4 +389,3 @@ class Database(bob.db.base.SQLiteDatabase):
         """Returns attack sample types available in the database"""
 
         return File.speech_choices
-
